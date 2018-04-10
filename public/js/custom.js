9c3fc32864a4dde56358621bafeb4d57942b9433
Jordan Hoover <jordan@hoovermld.com>
2018-04-10 14:15:05 -0500

    Added js url parsing functionality and better form validation

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,7 @@
 var luminateForms = (function($) {
   const form = jQuery("#luminate-form-" + php_vars['form_id']);
   const inputArr = form.find('input');
+  const emailRe = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
   function alertEmpties() {
     let formNotEmpty = true;
@@ -9,27 +10,28 @@ var luminateForms = (function($) {
       let e = $(this);
 
       if (e.attr("id").includes("cons")) {
-        let elemVal = e.attr("id").split("_")
-        elemVal.shift()
+        let elemVal = e.attr("id").split("_");
+        elemVal.shift();
         elemVal = elemVal.join(" ");
 
-        if (e.val() === "" || e.val().toLowerCase() === elemVal) {
-          e.addClass('form-field-empty');
+        if (
+          e.val() === "" ||
+          e.val().toLowerCase() === elemVal ||
+          !validateField('name', e.val(), /[a-zA-Z]/) ||
+          !validateField('email', e.val(), emailRe) ||
+          !validateField('zip', e.val(), /(^\d{5}$)|(^\d{5}-\d{4}$)/)
+        ) {
           formNotEmpty = false;
+          e.addClass('form-field-empty');
         }
 
-        //test email field
-        if (elemVal.includes('email')) {
-          if (!validateEmail(e.val())) {
-            formNotEmpty = false;
-            e.addClass('form-field-empty');
+        function validateField(partialFieldName, formVal, re) {
+          if (typeof formVal === "string") {
+            formVal = formVal.toLowerCase();
           }
-        }
-        if (elemVal.includes('zip')) {
-          re = /(^\d{5}$)|(^\d{5}-\d{4}$)/;
-          if (!re.test((e.val()))) {
-            formNotEmpty = false;
-            e.addClass('form-field-empty');
+
+          if (elemVal.includes(partialFieldName)) {
+            return re.test((formVal));
           }
         }
       }
@@ -40,17 +42,13 @@ var luminateForms = (function($) {
       console.log("Invalid form submission");
     }
 
+
+
     return formNotEmpty;
   }
 
   function removeErrorClass() {
     form.find('input').removeClass('form-field-empty');
-  }
-
-  function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    return re.test(email.toLowerCase());
   }
 
   return {
@@ -71,3 +69,22 @@ luminateForms.currentForm.submit(function(e) {
 luminateForms.currentForm.focus(function() {
   luminateForms.removeErrors();
 });
+
+(function($) {
+	function getURLParameter(name) {
+	  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+	}
+
+	if ( $('.luminate-forms').length ) {
+		var source = getURLParameter('src');
+		var sub_source = getURLParameter('sub_src');
+
+		if (source) {
+			$('.luminate-forms #source').val(source);
+		}
+
+		if (sub_source) {
+			$('.luminate-forms #sub-source').val(sub_source);
+		}
+	}
+})(jQuery);
